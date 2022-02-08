438f870393e9f4b43896bead8c2c9d765622e742
William Burke <williamtburke311@gmail.com>
2022-02-08 11:28:29 -0800

    first commit

---
+++ Arrays.py
@@ -0,0 +1,15 @@
+# practice with Arrays
+
+def array_test():
+    arr = [10, 20, 30, 40, 50]
+
+    arr.index(30)
+
+    print()
+
+
+def main():
+    array_test()
+
+if __name__ == "__main__":
+    main()
